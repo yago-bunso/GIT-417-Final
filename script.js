f0742903af1d5afef312b71485ebbc723eab12fe
yago-bunso <hyago@asu.edu>
2022-11-25 22:37:55 -0700

    updated many font styles
    updated many font styles:
    next: format product display section

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 //click event - canvas 2
-document.querySelector("#toggleSwitch image").addEventListener("click", function(){
-    document.querySelector("#toggleSwitch img").classList.toggle("hidden");
-  });
+//document.querySelector("#toggleSwitch img").addEventListener("click", function(){
+    //document.querySelector("#toggleSwitch img").//classList.toggle("hidden");
+ // });
 
-  
+ ////canvas 9 - background color switcher
+//let colors = ["red", "orange", "yellow", "green", "blue", "purple"];
+//let circle = document.querySelector("#canvas9 div");
+
+//random number generator
+//function getRandomInt(min, max) {
+  //min = Math.ceil(min);
+ //max = Math.floor(max);
+  //return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+//}
+
+//function to change the color of the circle on click
+//function changeColor(){
+  //let newColor = colors[getRandomInt(0, 6)];
+  //circle.style.backgroundColor = newColor;
+//}
+
+//add event listener to button
+//document.querySelector("#canvas9 button").addEventListener("click", changeColor);
+
