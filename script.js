4e5503e24bfc4bf24d305d6b279d32a6ecf73266
yago-bunso <hyago@asu.edu>
2022-11-28 22:45:51 -0700

    Messing with js
    Messing with js

--- script.js
+++ script.js
@@ -1,25 +1,9 @@
-//click event - canvas 2
-//document.querySelector("#toggleSwitch img").addEventListener("click", function(){
-    //document.querySelector("#toggleSwitch img").//classList.toggle("hidden");
- // });
+"use strict";
 
- ////canvas 9 - background color switcher
-//let colors = ["red", "orange", "yellow", "green", "blue", "purple"];
-//let circle = document.querySelector("#canvas9 div");
+//I'm going to use a checkbox method to toggle light and dark mode. 
+let darkToggle = 
+document.querySelector('#darkToggle');
 
-//random number generator
-//function getRandomInt(min, max) {
-  //min = Math.ceil(min);
- //max = Math.floor(max);
-  //return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
-//}
-
-//function to change the color of the circle on click
-//function changeColor(){
-  //let newColor = colors[getRandomInt(0, 6)];
-  //circle.style.backgroundColor = newColor;
-//}
-
-//add event listener to button
-//document.querySelector("#canvas9 button").addEventListener("click", changeColor);
-
+darkToggle.addEventListener('change', ()=>{
+document.body.classList.toggle('dark');
+})
