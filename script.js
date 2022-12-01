4e0e6239414efc682c19ff76916c08e268d63718
yago-bunso <hyago@asu.edu>
2022-11-30 20:35:29 -0700

    JUMP FOR JOY
    the product buttons work

--- script.js
+++ script.js
@@ -2,14 +2,10 @@
 
 //Light and Dark mode toggle
 function darkMode() {
-  let body = document.getElementById("background-color");
+  let body = document.getElementById("body-color");
   body.classList.toggle("dark-mode");
   let h2 = document.getElementById("h2");
   h2.classList.toggle("dark-mode-h2");
-
-
-
-
 
   let button = document.getElementById("toggleButton");
   if (button.innerHTML === "Dark mode"){
@@ -25,6 +21,20 @@ function darkMode() {
   }
 }
 
+//let productFigImg = document.querySelector("figure img");
+//let displayH3 = document.querySelectorAll("#product-h3 h3");
+//let displayNutrition = document.querySelectorAll("#product-nutrition p");
+//let displayDescription = document.querySelectorAll("grid-child-element-paragraph p");
+//let displayImage = document.querySelectorAll("grid-child-element-image img");
 
 
+//By default, CBL is shown. On click on the thumbnail, CBL content should display. 
 
+function displayProduct(){
+let CBLbutton = document.querySelector("#CBL-p1 img");
+CBLbutton.addEventListener("click", function(){
+  document.getElementById("CBL-desc").classList.remove("hidden");
+  document.getElementById("CBL-h3").classList.remove("hidden");
+  document.getElementById("CBL-img").classList.remove("hidden");
+})
+}
